2f6848a74046a04d871360bdb4883a73cb67efa3
ajenkins <alex.jenkins@campgemini.com>
2024-05-27 01:07:43 +1000

    added main file for core example flow

---
+++ __main__.py
@@ -0,0 +1,43 @@
+import logging
+
+from dotenv import load_dotenv
+
+from relevance_pipeline.utils.rag_pipelines import get_all_evaluators, get_gpt_inmemory_store
+from relevance_pipeline.query_ai.open_ai_gpt import create_openai_answer_engine, convert_context_nodes_to_context_model
+
+logging.basicConfig(level=logging.ERROR)
+logging.captureWarnings(True)
+load_dotenv()
+
+def main():
+    question = "I have Third Party Fire & Theft insurance under the allianz policy. What is the most they will repay me for my car?"
+
+    store = get_gpt_inmemory_store()
+
+    context_nodes = store.retrieve_from_knowledge_store(question)
+    context_model = convert_context_nodes_to_context_model(context_nodes)
+
+    openai_generator = create_openai_answer_engine()
+    answer_model = openai_generator.answer_question_with_context(question, context_model)
+
+    evaluators = get_all_evaluators()
+
+    context_strs = [node.str_context for node in context_model]
+    evaluations = []
+    for evaluator in evaluators:
+        eval = evaluator.evaluate(question, answer_model.answer, context_strs)
+        evaluations.append(eval)
+
+    return question, context_model, answer_model, evaluations
+
+
+if __name__ == "__main__":
+    q, c, a, e = main()
+    print(f'## Question:\n{q}\n')
+    print(f'## Context:')
+    for node in c:
+        print(f'  - {node}')
+    print(f'## Answer:\n{a}\n')
+    print(f'## Evaluation(s):')
+    for ev in e:
+        print(f'  - {ev}')
