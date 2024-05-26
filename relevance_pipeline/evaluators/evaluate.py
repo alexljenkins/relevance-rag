af7e14c7ef75d16ebf6e6860c0f28edd48b4a2ab
ajenkins <alex.jenkins@campgemini.com>
2024-05-27 01:02:18 +1000

    expanded on evaluators and what else we could add

--- relevance_pipeline/evaluators/evaluate.py
+++ relevance_pipeline/evaluators/evaluate.py
@@ -15,3 +15,15 @@ class EvaluatorFactory:
     @staticmethod
     def get_evaluator(evaluator_type: EvaluatorType) -> BaseEvaluator:
         return evaluator_type.value()
+
+"""
+TODO: additional evaluators could be:
+- speed/latency
+- cost and token usage
+- context relevance scores
+- human eval / ground truth
+- generated questions from specific context chunks marking that chunk as the answer block (would require a persistent store though)
+    (see: https://docs.llamaindex.ai/en/stable/module_guides/evaluating/usage_pattern/?h=deepevalanswerrelevancyevaluator#question-generation)
+- rank (position) of the answer context block in the retrieved contexts (ie was it the top result or not)
+- compare context to what an LLM extracts as the best answer context area - and compare the overlap (since context windows are massive now)
+"""
